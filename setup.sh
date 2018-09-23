3f2207701ad8a48d2a9de1c854a54d180dcf97bd
Jake Vogel <jacobwvogel@gmail.com>
2018-09-22 22:44:25 -0400

    cleaned up a bit. Addressed scipy 1 issue

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 conda create -n nilearn_tutorial python=3.6
 source activate nilearn_tutorial
 pip install -U --user nilearn
-conda install scipy matplotlib scikit-learn jupyter
+conda install scipy matplotlib scikit-learn jupyter pandas
 pip install -U --user scipy
