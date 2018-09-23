c56052e9f904e5a78f1cca99d65734b1bf521a10
Jake Vogel <jacobwvogel@gmail.com>
2018-09-23 18:35:50 -0400

    address scipy duplicate

--- setup.bat
+++ setup.bat
@@ -3,5 +3,5 @@ title Setting up nilearn tutorial
 conda create -n nilearn_tutorial python=3.6
 activate nilearn_tutorial
 pip install -U --user nilearn
-conda install scipy matplotlib scikit-learn jupyter pandas
+conda install matplotlib scikit-learn jupyter pandas
 pip install -U --user scipy
